7786ab4e9d6744ad1a65f278c28aabbf71004543
Shahetul Chisty <shahetulchisty@Shahetuls-MacBook-Pro.local>
2021-11-14 15:48:42 -0800

    New Project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+
+window.addEventListener("click", e =>{
+    if(e.target.id === "hamburger" || e.target.className === "line"){
+        
+    }
+})
