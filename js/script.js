293bcf0cc0c53ff4f1fdadb9656216c7e0ff4a04
Shahetul Chisty <shahetulchisty@Shahetuls-MacBook-Pro.local>
2021-11-14 19:12:08 -0800

    Fade in effect on landing page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const hamburger = document.getElementById("hamburger");
+const mainItems = document.getElementById("main-items");
 
 window.addEventListener("click", e =>{
     if(e.target.id === "hamburger" || e.target.className === "line"){
-        
+        console.log("click");
     }
 })
+
+window.addEventListener("load", e =>{
+    $("#main-items").animate({"opacity": "1"}, 2500);
+    console.log("loaded");
+});
