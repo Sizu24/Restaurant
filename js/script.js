28e4f868797a0a2a9d02f4d4acdef99ad094854d
Shahetul Chisty <shahetulchisty@Shahetuls-MacBook-Pro.local>
2021-11-15 11:45:38 -0800

    update js file to match gh pages code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const hamburger = document.getElementById("hamburger");
+const mainItems = document.getElementById("main-items");
 
 window.addEventListener("click", e =>{
     if(e.target.id === "hamburger" || e.target.className === "line"){
-        
+        console.log("click");
     }
 })
+
+window.addEventListener("load", () =>{
+    $("#main-items").animate({"opacity": "1"}, 3000);
+});
