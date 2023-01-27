a713cf07a13ad73aaa7096b24928f1aaab1c83fe
Shah <sizu24@me.com>
2023-01-27 01:44:43 -0800

    Add swiper js and slider component files

--- wp-content/themes/chisty-theme/template-parts/text-media.php
+++ wp-content/themes/chisty-theme/template-parts/text-media.php
@@ -2,16 +2,17 @@
   <div class="text-media__container">
     <div class="text-media__text-content">
       <p class="text-media__eyebrow">
-        Our ingredients are made from scratch
+        All Natural
       </p>
       <h2 class="text-media__heading">
-        All Natural.
+        Our ingredients are made from scratch.
       </h2>
       <div class="text-media__rich-text">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         Mattis vulputate enim nulla aliquet porttitor lacus.
         Cursus mattis molestie a iaculis at erat pellentesque.
       </div>
+      <a class="text-media__button button" href="">Button</a>
     </div>
     <div class="text-media__media-content">
       <img class="text-media__image" src="/wp-content/themes/chisty-theme/src/assets/images/pexels-flo-dahm-1137473.jpg" alt="">
